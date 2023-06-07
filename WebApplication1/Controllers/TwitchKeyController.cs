6d4094298e1d3022d23afadb24c54227a0f9cda1
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-06-07 11:01:20 +0200

    🛠️ Empowering CRUD Operations! Equipped our application with the mighty powers of GET, DELETE, and PUT, enabling seamless data retrieval, deletion, and modification for an enhanced user experience 🚀🔍🗑️🖊️

--- WebApplication1/Controllers/TwitchKeyController.cs
+++ WebApplication1/Controllers/TwitchKeyController.cs
@@ -27,9 +27,16 @@ namespace TwitchKeyAPI.Controllers
 
         // GET api/<TwitchKeyController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get([FromRoute] Guid id)
         {
-            return "value";
+            var twitchKey = await dbContext.TwitchKey.FindAsync(id);
+
+            if (twitchKey == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(twitchKey);
         }
 
         // POST api/<TwitchKeyController>
@@ -53,14 +60,39 @@ namespace TwitchKeyAPI.Controllers
 
         // PUT api/<TwitchKeyController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put([FromRoute] Guid id, AddTwitchKeyRequest _twitchKeyRequest) // would normally make another file, isnt necesairy for this example
         {
+            var twitchKey = await dbContext.TwitchKey.FindAsync(id);
+
+            if (twitchKey == null)
+            {
+                return NotFound();
+            }
+
+            twitchKey.AccessToken = _twitchKeyRequest.AccessToken;
+            twitchKey.ExpiresIn = _twitchKeyRequest.ExpiresIn;
+            twitchKey.TokenType= _twitchKeyRequest.TokenType;
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok(twitchKey);
         }
 
         // DELETE api/<TwitchKeyController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
+            var twitchKey = await dbContext.TwitchKey.FindAsync(id);
+
+            if (twitchKey == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(twitchKey);
+            dbContext.SaveChanges();
+
+            return Ok(twitchKey);
         }
     }
 }
